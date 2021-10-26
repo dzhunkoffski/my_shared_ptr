6a22d785147b21ebcb10bba34b0853b9847f1af3
Kirill Dzhunkowski <dzhunkofffski@gmail.com>
2021-10-26 22:42:41 +0300

    Start

---
+++ cmake-build-debug/CMakeFiles/my_shared_ptr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_shared_ptr.dir/main.cpp.o"
+  "my_shared_ptr"
+  "my_shared_ptr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_shared_ptr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
